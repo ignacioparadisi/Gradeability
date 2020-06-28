f8825da2fb0da0f034bb89ac57b70190de5c05fb
Ignacio Paradisi <ignacioparadisi@gmail.com>
2020-06-28 19:33:52 -0400

    Create swipe gesture to show delete button

--- Gradeability/Modules/Terms/Collection View Cells/GradableCollectionViewCell.swift
+++ Gradeability/Modules/Terms/Collection View Cells/GradableCollectionViewCell.swift
@@ -12,7 +12,14 @@ class GradableCollectionViewCell: UICollectionViewCell, ReusableView {
     private let primaryCellView: GradableCellPrimaryView = GradableCellPrimaryView()
     private let secondaryCellView = GradableCellSecondaryView()
     private var primaryViewBottomAnchor: Anchor?
-
+    private var contentViewTrailingSuperviewAnchor: NSLayoutConstraint?
+    private var contentViewTrailingButtonAnchor: NSLayoutConstraint?
+    var deleteButton: UIButton = UIButton()
+    private var isPanning: Bool = false
+    private var currentLocation: CGPoint = CGPoint(x: 0, y: 0)
+    
+    var panGesture: UIPanGestureRecognizer!
+    
     // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -24,8 +31,9 @@ class GradableCollectionViewCell: UICollectionViewCell, ReusableView {
     }
     
     func setupView() {
-        backgroundColor = UIColor(named: "cellSecondaryBackgroundColor")
-        layer.cornerRadius = 15
+        setupDeleteButton()
+        contentView.backgroundColor = UIColor(named: "cellSecondaryBackgroundColor")
+        contentView.layer.cornerRadius = 15
         contentView.addSubview(primaryCellView)
         primaryCellView.anchor
             .topToSuperview()
@@ -34,6 +42,28 @@ class GradableCollectionViewCell: UICollectionViewCell, ReusableView {
             .activate()
         primaryViewBottomAnchor = primaryCellView.anchor.bottomToSuperview()
         setupSecondaryView()
+        addGesture()
+    }
+    
+    func addGesture() {
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        panGesture.delegate = self
+        self.addGestureRecognizer(panGesture)
+    }
+    
+    private func setupDeleteButton() {
+        deleteButton.backgroundColor = .systemRed
+        deleteButton.setTitle("Delete", for: .normal)
+        deleteButton.tintColor = .white
+        deleteButton.layer.cornerRadius = 15
+        insertSubview(deleteButton, at: 0)
+        deleteButton.anchor
+            .topToSuperview()
+            .trailingToSuperview()
+            .bottomToSuperview()
+            .width(constant: 100)
+            .activate()
+        deleteButton.transform = CGAffineTransform(scaleX: 0, y: 0)
     }
     
     private func setupSecondaryView() {
@@ -60,5 +90,67 @@ class GradableCollectionViewCell: UICollectionViewCell, ReusableView {
         }
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if (panGesture.state == .changed) {
+            let locationX: CGFloat = min(panGesture.translation(in: self).x, currentLocation.x)
+            let width = self.contentView.frame.width
+            let height = self.contentView.frame.height
+            self.contentView.frame = CGRect(x: locationX, y: 0, width: width, height: height)
+            if abs(locationX / 100) <= 1 {
+                deleteButton.transform = CGAffineTransform(scaleX: abs(locationX / 100), y: abs(locationX / 100))
+            } else if abs(locationX / 110) > 1 {
+                deleteButton.frame = CGRect(x: locationX + width + 10, y: 0, width: abs(locationX) - 10, height: height)
+            }
+        }
+    }
+    
+    @objc func onPan(_ gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            break
+        case .changed:
+            if gesture.velocity(in: self).x < 0 || currentLocation.x != 0{
+                isPanning = true
+            }
+            if !isPanning { return }
+            setNeedsLayout()
+        case .ended:
+            print("Ended")
+            if abs(panGesture.translation(in: self).x) > 110 {
+                currentLocation = CGPoint(x: -110, y: 0)
+                let width = self.contentView.frame.width
+                let height = self.contentView.frame.height
+                self.contentView.frame = CGRect(x: -110, y: 0, width: width, height: height)
+                self.deleteButton.frame = CGRect(x: width - 100, y: 0, width: 100, height: height)
+            } else {
+                currentLocation = CGPoint(x: 0, y: 0)
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.deleteButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+                    self.setNeedsLayout()
+                    self.layoutIfNeeded()
+                })
+            }
+            
+        default:
+            isPanning = false
+            UIView.animate(withDuration: 0.2, animations: {
+                self.deleteButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+                self.setNeedsLayout()
+                self.layoutIfNeeded()
+            })
+        }
+    }
+    
+}
+
+extension GradableCollectionViewCell: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return abs((panGesture.velocity(in: panGesture.view)).x) > abs((panGesture.velocity(in: panGesture.view)).y)
+    }
 }
 
